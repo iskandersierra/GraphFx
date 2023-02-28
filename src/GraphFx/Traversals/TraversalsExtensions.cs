ce5fd8b2219f58ac7c1600449f4f4af03d02ac5f
Iskander <iskander.sierra@gmail.com>
2023-02-28 16:28:40 +0100

    Started Traversals namespace

---
+++ src/GraphFx/Traversals/TraversalsExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace GraphFx.Traversals;
+
+public static class TraversalsExtensions
+{
+    public static IEnumerable<TVertex> DepthFirstSearchRecursive<TVertex>(
+        this IIncidenceGraph<TVertex> graph)
+        where TVertex : notnull
+    {
+        var visited = new HashSet<TVertex>(graph.VertexEqualityComparer);
+
+        IEnumerable<TVertex> Loop(TVertex vertex)
+        {
+            if (visited.Add(vertex))
+            {
+                yield return vertex;
+                foreach (var adjacentVertex in graph.OutgoingEdges(vertex))
+                {
+                    foreach (var result in Loop(adjacentVertex))
+                    {
+                        yield return result;
+                    }
+                }
+            }
+        }
+
+        foreach (var vertex in graph.SeedVertices)
+        {
+            foreach (var result in Loop(vertex))
+            {
+                yield return result;
+            }
+        }
+    }
+}
